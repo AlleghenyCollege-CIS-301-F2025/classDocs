0fef67548edf2b62be3cc1f860336937e1144db5
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2025-09-07 21:46:39 -0400

    adding 03 material

---
+++ src/begin.R
@@ -0,0 +1,9 @@
+# Use this code at the start of each piece of code. The lines clear memory and plots to prevent
+# interference of old variable values with the new values that your updated code may produce. 
+
+rm(list = ls()) # clear out the variables from memory to make a clean execution of the code.
+
+# If you want to remove all previous plots and clear the console, run the following two lines.
+graphics.off() # clear out all plots from previous work.
+
+cat("\014") # clear the console
